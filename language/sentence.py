d6393bbecaebf226a3e0b8c8be020d705eb8f0cc
Mohammad Issawi <2000issawi@gmail.com>
2024-01-03 20:26:25 +0300

    Add Sentence.problems attribute

--- language/sentence.py
+++ language/sentence.py
@@ -4,5 +4,6 @@ from tagger.BNC import PTB2BNC
 class Sentence:
     def __init__(self,sentence:str):
         self.raw_sentence=sentence
+        self.problems=[]
     def get_bnc_tagged(self):
         return PTB2BNC(penn_treebank_tag(self.raw_sentence))
