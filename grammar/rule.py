170a9733c1daee959d9d417c766c9ae74f05d685
Mohammad Issawi <2000issawi@gmail.com>
2024-01-04 02:44:57 +0300

    Implement Rule.first_matched_window

--- grammar/rule.py
+++ grammar/rule.py
@@ -38,3 +38,14 @@ class Rule:
             if re.match(Rule.pattern_re_string(self.incorrect_pattern), sub_tagged_str):
                 matches.append((i, con_len + i))
         return matches
+
+    def first_matched_window(self,sentence:Sentence):
+        tagged = sentence.get_bnc_tagged()
+        con_len = len(self.construct)
+        for i in range(len(tagged) - con_len + 1):
+            sub_tagged = tagged[i:con_len + i]
+            # TODO take all possible POS tags into account
+            sub_tagged = [x[0] if self.construct[i] == 'w' else x[1][0] for i, x in enumerate(sub_tagged)]
+            sub_tagged_str = str.join(' ', sub_tagged)
+            if re.match(Rule.pattern_re_string(self.incorrect_pattern), sub_tagged_str):
+                return (i,i+con_len)
