31dd6eedbd51247f2e89ca71342325ea95eba253
Mohammad Issawi <2000issawi@gmail.com>
2024-01-03 19:58:55 +0300

    Create PTB to BNC tags map

---
+++ tagger/BNC.py
@@ -0,0 +1,41 @@
+
+_ptb2bnc_map ={
+    'CC' :['CJC'],
+    'CD':['CRD'],
+    'DT':['DT0','AT0'],
+    'EX':['EX0'],
+    'FW':['UNC'],
+    'IN':['PRF','PRP'],
+    'JJ':['AJ0'],
+    'JJR':['AJC'],
+    'JJS':['AJS'],
+    'LS':['CRD'],
+    'MD':['VM0'],
+    'NN':['NN0','NN1'],
+    'NNS':['NN2'],
+    'NNP':['NP0'],
+    'NNPS':['NP0'],
+    'PDT':['DT0'],
+    'POS':['POS'],
+    'PRP':['PNP'],
+    'PRP$':['PNP'],
+    'RB':['AV0'],
+    'RBR':['AV0','AJC'],
+    'RBS':['AV0','AJS'],
+    'RP':['AVP'],
+    'TO':['TO0'],
+    'UH':['UNC'],
+    'VB':['VVI'],
+    'VBD':['VVD'],
+    'VBG':['VVG'],
+    'VBN':['VVN'],
+    'VBP':['VVI'],
+    'VBZ':['VVZ'],
+    'WDT':['DTQ'],
+    'WP':['PNQ'],
+    'WP$':['DTQ'],
+    'WRB':['AVQ']
+}
+
+def PTB2BNC(penn_treebank_tagged_sentence):
+    pass
