b65e89f8bc1f9102c87a1d4c5d9c2d1c07937ff9
Maias <maias-alakramy01@hotmail.com>
2024-01-04 16:38:44 +0300

    break when no changes

--- grammar/checker.py
+++ grammar/checker.py
@@ -8,11 +8,16 @@ class Checker:
 
     def check(self, sentence: Sentence, max_corrects=10):
         for _ in range(max_corrects):
+            made_changes = False
             for rule in self.rules:
                 i = rule.first_matched_window(sentence)
                 if i != -1:
+                    made_changes = True
                     correct_window = rule.correct_window(sentence, i)
                     sentence = Sentence(sentence.subsititue(i, rule.con_len, correct_window.raw_sentence))
-            print(sentence.raw_sentence)
+            if not(made_changes):
+                break
+            else:
+                print(sentence.raw_sentence)
 
         return sentence
