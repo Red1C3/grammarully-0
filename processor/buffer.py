6bba70b8fc10e2be5ceea867debe46f10f5d99ce
Mohammad Issawi <2000issawi@gmail.com>
2024-01-05 21:33:45 +0300

    Create buffering only by pushbacking (it will support editing mid-text later)

---
+++ processor/buffer.py
@@ -0,0 +1,28 @@
+from grammar.checker import Checker
+from language.sentence import Sentence
+
+
+class Buffer:
+    STOP_TOKENS = ('.', '\n')
+
+    def __init__(self, max_checker_iterations=10, verbose_checker=False):
+        self._buffer = ''
+        self._checker = Checker()
+        self._max_checker_iterations = max_checker_iterations
+        self._verbose_checker = verbose_checker
+
+    # Second return value is the number of processed chars (they won't be processed by the buffer any more)
+    # it is useful when the buffer processes only a part of it
+    def write_char(self, char: str):
+        if char in Buffer.STOP_TOKENS and len(self._buffer) == 0:
+            self._buffer = ''
+            return char, 1
+        if char in Buffer.STOP_TOKENS:
+            processed_sent = self._checker.check(Sentence(self._buffer), self._verbose_checker,
+                                                 self._max_checker_iterations)
+            self._buffer = ''  # Reset buffer
+            processed_str = processed_sent.get_striped() + char
+            return processed_str, len(processed_str)
+        else:
+            self._buffer += char
+            return self._buffer, 0
