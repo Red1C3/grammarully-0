300da7eccc9d8502ea848fcbc8f86c6ef60c541f
Mohammad Issawi <2000issawi@gmail.com>
2024-01-05 18:01:58 +0300

    Add a test

--- test/tests.py
+++ test/tests.py
@@ -60,7 +60,8 @@ _test_pairs = [
     ('the tagger will return less as five tags',
      'the tagger will return less than five tags'),
     ('this cat is larger then me', 'this cat is larger than me'),
-    ('this cat is smaller as me', 'this cat is smaller than me')
+    ('this cat is smaller as me', 'this cat is smaller than me'),
+    ('your gone','you are gone')
 ]
 
 
