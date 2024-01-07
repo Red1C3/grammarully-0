5a368bcd665366cac367ada364a17847ca81205c
Mohammad Issawi <2000issawi@gmail.com>
2024-01-07 15:52:30 +0300

    Facilitate testing editor mode

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+#!./.venv/bin/python
+
+import curses
+import sys
+
 from language.sentence import Sentence
 from grammar.rule import Rule
 from grammar.checker import Checker
 from test.tests import run_tests
+from processor.window import Window
 def main():
     run_tests()
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'editor':
+            curses.wrapper(Window().loop)
+    else:
+        main()
