7e51544b6f7f7723c99da65e0c86729aeecd066f
Mohammad Issawi <2000issawi@gmail.com>
2024-01-08 20:19:01 +0300

    Save file inplace and create file if not exist

--- processor/window.py
+++ processor/window.py
@@ -14,19 +14,30 @@ class Window:
         self.buffer_cache = ''
         if len(argv) > 2:
             self.filepath = argv[2]
-            with open(argv[2]) as txt_file:
-                for line in txt_file:
-                    for c in line:
-                        self.buffer_cache, submit = self.buffer.write_char(c)
-                        if submit > 0:
-                            self.text += self.buffer_cache[:submit]
-                            self.buffer_cache = ''
+            try:
+                with open(argv[2]) as txt_file:
+                    for line in txt_file:
+                        for c in line:
+                            self.buffer_cache, submit = self.buffer.write_char(
+                                c)
+                            if submit > 0:
+                                self.text += self.buffer_cache[:submit]
+                                self.buffer_cache = ''
+            except FileNotFoundError:
+                pass
 
     def loop(self, w: window):
         self.w = w
         self.w.addstr(self.text)
         while True:
-            c = w.getch()
+            try:
+                c = w.getch()
+            except KeyboardInterrupt:
+                if len(argv) > 2:
+                    f = open(self.filepath, 'w')
+                    f.write(self.text+self.buffer_cache)
+                    f.close()
+                return
             if isalnum(c) or ispunct(c) or isspace(c):
                 self.buffer_cache, submit = self.buffer.write_char(chr(c))
                 if submit > 0:
