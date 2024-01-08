0bd6fd193b8e6ecf13b094d450a7f9f03087b1d2
Mohammad Issawi <2000issawi@gmail.com>
2024-01-08 21:49:01 +0300

    Handle removing chars from the end and preserving spaces at the beginning of a sentence

--- processor/buffer.py
+++ processor/buffer.py
@@ -16,19 +16,22 @@ class Buffer:
     def write_char(self, char: str):
         if char in Buffer.STOP_TOKENS and len(self._buffer) == 0:
             self._buffer = ''
-            return char, 1
+            return char, 1, False
         if char in Buffer.STOP_TOKENS:
+            starts_with_space = True if self._buffer[0] == ' ' else False
             processed_sent = self._checker.check(Sentence(self._buffer), self._verbose_checker,
                                                  self._max_checker_iterations)
             self._buffer = ''  # Reset buffer
             processed_str = processed_sent.get_striped() + char
-            return processed_str, len(processed_str)
+            processed_str = processed_str.lstrip()
+            return processed_str, len(processed_str), starts_with_space
         else:
             self._buffer += char
-            return self._buffer, 0
+            return self._buffer, 0, False
 
     def pop_char(self):
         self._buffer = self._buffer[:-1]
+        return self._buffer
 
     def set_without_processing(self, text):
         self._buffer = text
--- processor/window.py
+++ processor/window.py
@@ -18,9 +18,11 @@ class Window:
                 with open(argv[2]) as txt_file:
                     for line in txt_file:
                         for c in line:
-                            self.buffer_cache, submit = self.buffer.write_char(
+                            self.buffer_cache, submit, add_space = self.buffer.write_char(
                                 c)
                             if submit > 0:
+                                if add_space:
+                                    self.text += ' '
                                 self.text += self.buffer_cache[:submit]
                                 self.buffer_cache = ''
             except FileNotFoundError:
@@ -41,10 +43,26 @@ class Window:
                     f.close()
                 return
             if isalnum(c) or ispunct(c) or isspace(c):
-                self.buffer_cache, submit = self.buffer.write_char(chr(c))
+                self.buffer_cache, submit, add_space = self.buffer.write_char(chr(c))
                 if submit > 0:
+                    if add_space:
+                        self.text += ' '
                     self.text += self.buffer_cache[:submit]
                     self.buffer_cache = ''
+            if c == curses.KEY_BACKSPACE:
+                if len(self.buffer_cache) > 0:
+                    self.buffer_cache = self.buffer.pop_char()
+                else:
+                    self.text = self.text[:-1]
+                    stop_token_index = -1
+                    for t in Buffer.STOP_TOKENS:
+                        try:
+                            stop_token_index = max(stop_token_index, self.text.rindex(t))
+                        except ValueError:
+                            pass
+                    if stop_token_index != -1:
+                        self.buffer_cache = self.buffer.set_without_processing(self.text[stop_token_index + 1:])
+                        self.text = self.text[:stop_token_index + 1]
             w.erase()
             w.addstr(self.text + self.buffer_cache)
             self.w.refresh()
