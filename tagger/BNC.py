4303b9560c28ed2297d532050a5d10c5bc24d2f7
Maias <maias-alakramy01@hotmail.com>
2024-01-09 14:27:30 +0300

    one tense is enough

--- tagger/BNC.py
+++ tagger/BNC.py
@@ -76,9 +76,7 @@ def enhance_treebank_tag(word, tbt):
         # This has increased the complexity A LOT
         elif len(sys.argv)>1 and  sys.argv[1] == 'editor':
             v_tenses=tenses(word)
-            if len(v_tenses)==1:
-                return tense_to_tag(v_tenses[0]),False
-            tags=list(set(chain(*tags,*map(tense_to_tag,v_tenses))))
+            tags=list(set(chain(*tags,*map(tense_to_tag,[v_tenses[0]]))))
     return tags, True
 
 def tense_to_tag(tense_tuple):
